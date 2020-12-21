a1dbe95973662472aa4afe5ebcf41dccba792f3d
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2020-12-21 05:29:44 +0300

    Added clickable starring, fixed styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>, document.getElementById('root')
+ReactDOM.render(<App />, 
+    document.getElementById('root')
 );
