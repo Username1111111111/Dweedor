2ccc234fed604f27694c1323335b434f2c2e6452
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2021-01-06 20:31:07 +0300

    Forms, events, etc

--- src/app-header/app-header.js
+++ src/app-header/app-header.js
@@ -1,31 +1,32 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import './app-header.css';
 
-const Header = styled.div`
-    display: flex;
-    align-items: flex-end;
-    justify-content: space-between;
-    h1 {
-        font-size: 26px;
-        color: ${props => props.colored ? 'red' : 'black'};
-        :hover {
-            color: blue;
-        }
-    }
-    h2 {
-        font-size: 1.2rem;
-        color: grey;
-    }
-`;
+// const Header = styled.div`
+//     display: flex;
+//     align-items: flex-end;
+//     justify-content: space-between;
+//     h1 {
+//         font-size: 26px;
+//         color: ${props => props.colored ? 'red' : 'black'};
+//         :hover {
+//             color: blue;
+//         }
+//     }
+//     h2 {
+//         font-size: 1.2rem;
+//         color: grey;
+//     }
+// `;
 
-const AppHeader = () => {
+const AppHeader = ({liked, allPosts}) => {
     return (
-        // <div className="app-header d-flex">
-        <Header as='a' colored>
+        <div className="app-header d-flex">
+            {/* <Header as='a' colored> */}
             <h1>AccountName</h1>
-            <h2>5 записей, из них понравилось 0</h2>
-        </Header>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
+        </div>
     );
 };
 
--- src/post-list/post-list.js
+++ src/post-list/post-list.js
@@ -1,26 +1,28 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import PostListItem from '../post-list-item';
-import { ListGroup } from 'reactstrap';
+// import { ListGroup } from 'reactstrap';
 import './post-list.css';
 
-const PostList = ({posts, onDelete}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
     const elements = posts.map( (post) => {
         const {id, ...itemProps} = post;
         return (
-            <li key= {id} className='list-group-item'>
+            <li key={id} className='list-group-item'>
                 <PostListItem 
                     {...itemProps}
-                    onDelete={ () => onDelete(id)} 
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)} 
                 />
             </li>
         );
     });
 
     return (
-        <ListGroup className="app-list">
+        <ul className="app-list">
             {elements}
-        </ListGroup>
+        </ul>
     );
 };
 
